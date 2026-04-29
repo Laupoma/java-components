57b4b451159fcb1a1acd7191a2219c8944a4e6b7
Laupoma <lautaropoma45@gmail.com>
2026-04-29 09:49:37 -0300

    PIOT-GDA-05-002: SystemPerformanceManager updated with SystemPerformanceData and listener callback

--- src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
+++ src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
@@ -12,6 +12,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
@@ -19,6 +20,7 @@ import programmingtheiot.common.ConfigConst;
 import programmingtheiot.common.ConfigUtil;
 import programmingtheiot.common.IDataMessageListener;
 import programmingtheiot.common.ResourceNameEnum;
+
 import programmingtheiot.data.SystemPerformanceData;
 
 /**
@@ -28,17 +30,21 @@ import programmingtheiot.data.SystemPerformanceData;
 public class SystemPerformanceManager
 {
 	// static
-    // Comando: Logger
-    // Qué hace: Crea el grabador de bitácora para esta clase.
-    private static final Logger _Logger = Logger.getLogger(SystemPerformanceManager.class.getName());
+	
+	private static final Logger _Logger = Logger.getLogger(SystemPerformanceManager.class.getName());
 	
 	private int pollRate = ConfigConst.DEFAULT_POLL_CYCLES;
+	
 	private ScheduledExecutorService schedExecSvc = null;
 	private SystemCpuUtilTask sysCpuUtilTask = null;
 	private SystemMemUtilTask sysMemUtilTask = null;
-
+	
 	private Runnable taskRunner = null;
 	private boolean isStarted = false;
+	
+	private String locationID = ConfigConst.NOT_SET;
+	private IDataMessageListener dataMsgListener = null;
+	
 	
 	// constructors
 	
@@ -51,15 +57,19 @@ public class SystemPerformanceManager
 		this.pollRate =
 			ConfigUtil.getInstance().getInteger(
 				ConfigConst.GATEWAY_DEVICE, ConfigConst.POLL_CYCLES_KEY, ConfigConst.DEFAULT_POLL_CYCLES);
-	
+		
 		if (this.pollRate <= 0) {
 			this.pollRate = ConfigConst.DEFAULT_POLL_CYCLES;
 		}
-	
+		
+		this.locationID =
+			ConfigUtil.getInstance().getProperty(
+				ConfigConst.GATEWAY_DEVICE, ConfigConst.DEVICE_LOCATION_ID_KEY, ConfigConst.NOT_SET);
+		
 		this.schedExecSvc   = Executors.newScheduledThreadPool(1);
 		this.sysCpuUtilTask = new SystemCpuUtilTask();
 		this.sysMemUtilTask = new SystemMemUtilTask();
-	
+		
 		this.taskRunner = () -> {
 			this.handleTelemetry();
 		};
@@ -72,40 +82,50 @@ public class SystemPerformanceManager
 	{
 		float cpuUtil = this.sysCpuUtilTask.getTelemetryValue();
 		float memUtil = this.sysMemUtilTask.getTelemetryValue();
-	
-		// NOTE: you may need to change the logging level to 'info' to see the message
-		_Logger.fine("CPU utilization: " + cpuUtil + ", Mem utilization: " + memUtil);
+		
+		_Logger.info("CPU utilization: " + cpuUtil + ", Mem utilization: " + memUtil);
+		
+		SystemPerformanceData spd = new SystemPerformanceData();
+		spd.setLocationID(this.locationID);
+		spd.setCpuUtilization(cpuUtil);
+		spd.setMemoryUtilization(memUtil);
+		
+		if (this.dataMsgListener != null) {
+			this.dataMsgListener.handleSystemPerformanceMessage(
+				ResourceNameEnum.GDA_SYSTEM_PERF_MSG_RESOURCE, spd);
+		}
 	}
 	
 	public void setDataMessageListener(IDataMessageListener listener)
 	{
+		if (listener != null) {
+			this.dataMsgListener = listener;
+		}
 	}
-
+	
 	public boolean startManager()
 	{
 		if (! this.isStarted) {
 			_Logger.info("SystemPerformanceManager is starting...");
-		
+			
 			ScheduledFuture<?> futureTask =
 				this.schedExecSvc.scheduleAtFixedRate(this.taskRunner, 1L, this.pollRate, TimeUnit.SECONDS);
-		
+			
 			this.isStarted = true;
 		} else {
 			_Logger.info("SystemPerformanceManager is already started.");
 		}
-	
+		
 		return this.isStarted;
 	}
-
+	
 	public boolean stopManager()
 	{
 		this.schedExecSvc.shutdown();
 		this.isStarted = false;
-	
+		
 		_Logger.info("SystemPerformanceManager is stopped.");
-	
+		
 		return true;
 	}
-	
 }
-
