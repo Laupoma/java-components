af2ba61382cb70ed184315e38b6b67e348556f2f
Laupoma <lautaropoma45@gmail.com>
2026-07-26 18:16:03 -0300

    PIOT-GDA-08-002: UpdateTelemetry and UpdateSystemPerformance resource handlers created

---
+++ src/main/java/programmingtheiot/gda/connection/handlers/UpdateTelemetryResourceHandler.java
@@ -0,0 +1,90 @@
+/**
+ * This class is part of the Programming the Internet of Things project.
+ */ 
+
+package programmingtheiot.gda.connection.handlers;
+
+import java.util.logging.Logger;
+
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+
+import programmingtheiot.common.ConfigConst;
+import programmingtheiot.common.IDataMessageListener;
+import programmingtheiot.common.ResourceNameEnum;
+
+import programmingtheiot.data.DataUtil;
+import programmingtheiot.data.SensorData;
+
+public class UpdateTelemetryResourceHandler extends CoapResource
+{
+	private static final Logger _Logger =
+		Logger.getLogger(UpdateTelemetryResourceHandler.class.getName());
+
+	private IDataMessageListener dataMsgListener = null;
+
+	public UpdateTelemetryResourceHandler(String resourceName)
+	{
+		super(resourceName);
+	}
+
+	public void setDataMessageListener(IDataMessageListener listener)
+	{
+		if (listener != null) {
+			this.dataMsgListener = listener;
+		}
+	}
+
+	@Override
+	public void handlePUT(CoapExchange context)
+	{
+		ResponseCode code = ResponseCode.NOT_ACCEPTABLE;
+
+		context.accept();
+
+		if (this.dataMsgListener != null) {
+			try {
+				String jsonData = new String(context.getRequestPayload());
+
+				SensorData sensorData =
+					DataUtil.getInstance().jsonToSensorData(jsonData);
+
+				this.dataMsgListener.handleSensorMessage(
+					ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, sensorData);
+
+				code = ResponseCode.CHANGED;
+			} catch (Exception e) {
+				_Logger.warning("Failed to handle PUT request. Message: " + e.getMessage());
+				code = ResponseCode.BAD_REQUEST;
+			}
+		} else {
+			_Logger.info("No callback listener for request. Ignoring PUT.");
+			code = ResponseCode.CONTINUE;
+		}
+
+		String msg = "Update telemetry data request handled: " + super.getName();
+		context.respond(code, msg);
+	}
+
+	@Override
+	public void handleGET(CoapExchange context)
+	{
+		context.accept();
+		context.respond(ResponseCode.CONTENT, "GET not fully implemented.");
+	}
+
+	@Override
+	public void handlePOST(CoapExchange context)
+	{
+		context.accept();
+		context.respond(ResponseCode.CREATED, "POST not fully implemented.");
+	}
+
+	@Override
+	public void handleDELETE(CoapExchange context)
+	{
+		context.accept();
+		context.respond(ResponseCode.DELETED, "DELETE not fully implemented.");
+	}
+}
