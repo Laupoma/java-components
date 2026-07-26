79a1ad6394a0fcce2a11643e9be52dd2dfdde51b
Laupoma <lautaropoma45@gmail.com>
2026-07-26 18:06:03 -0300

    PIOT-GDA-08-001: CoapServerGateway implemented and integrated with DeviceDataManager

--- src/main/java/programmingtheiot/gda/app/DeviceDataManager.java
+++ src/main/java/programmingtheiot/gda/app/DeviceDataManager.java
@@ -209,6 +209,14 @@ public class DeviceDataManager implements IDataMessageListener
 		if (this.sysPerfMgr != null) {
 			this.sysPerfMgr.startManager();
 		}
+
+		if (this.enableCoapServer && this.coapServer != null) {
+			if (this.coapServer.startServer()) {
+				_Logger.info("CoAP server started.");
+			} else {
+				_Logger.severe("Failed to start CoAP server. Check log file for details.");
+			}
+		}
 	}
 
 	public void stopManager()
@@ -229,6 +237,14 @@ public class DeviceDataManager implements IDataMessageListener
 				_Logger.info("Successfully disconnected MQTT client from broker.");
 			} else {
 				_Logger.severe("Failed to disconnect MQTT client from broker.");
+			}
+		}
+
+		if (this.enableCoapServer && this.coapServer != null) {
+			if (this.coapServer.stopServer()) {
+				_Logger.info("CoAP server stopped.");
+			} else {
+				_Logger.severe("Failed to stop CoAP server. Check log file for details.");
 			}
 		}
 	}
@@ -255,7 +271,7 @@ public class DeviceDataManager implements IDataMessageListener
 		}
 
 		if (this.enableCoapServer) {
-			// TODO: implement this in Lab Module 8
+			this.coapServer = new CoapServerGateway(this);
 		}
 
 		if (this.enableCloudClient) {
--- src/main/java/programmingtheiot/gda/connection/CoapServerGateway.java
+++ src/main/java/programmingtheiot/gda/connection/CoapServerGateway.java
@@ -1,64 +1,44 @@
 /**
  * This class is part of the Programming the Internet of Things project.
- * 
- * It is provided as a simple shell to guide the student and assist with
- * implementation for the Programming the Internet of Things exercises,
- * and designed to be modified by the student as needed.
  */ 
-
 package programmingtheiot.gda.connection;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.network.Endpoint;
+import org.eclipse.californium.core.network.interceptors.MessageTracer;
 import org.eclipse.californium.core.server.resources.Resource;
+
+import org.eclipse.californium.core.config.CoapConfig;
+import org.eclipse.californium.elements.config.UdpConfig;
 
 import programmingtheiot.common.ConfigConst;
 import programmingtheiot.common.IDataMessageListener;
 import programmingtheiot.common.ResourceNameEnum;
-import programmingtheiot.gda.connection.handlers.GenericCoapResourceHandler;
 
-/**
- * Shell representation of class for student implementation.
- * 
- */
 public class CoapServerGateway
 {
-	// static
-	
 	private static final Logger _Logger =
 		Logger.getLogger(CoapServerGateway.class.getName());
 	
-	// params
+	static {
+		CoapConfig.register();
+		UdpConfig.register();
+	}
 	
 	private CoapServer coapServer = null;
-	
 	private IDataMessageListener dataMsgListener = null;
 	
-	
-	// constructors
-	
-	/**
-	 * Constructor.
-	 * 
-	 * @param dataMsgListener
-	 */
 	public CoapServerGateway(IDataMessageListener dataMsgListener)
 	{
 		super();
-		
-		/*
-		 * Basic constructor implementation provided. Change as needed.
-		 */
 		
 		this.dataMsgListener = dataMsgListener;
 		
 		initServer();
 	}
-
-		
-	// public methods
 	
 	public void addResource(ResourceNameEnum resource)
 	{
@@ -71,20 +51,48 @@ public class CoapServerGateway
 	
 	public void setDataMessageListener(IDataMessageListener listener)
 	{
+		if (listener != null) {
+			this.dataMsgListener = listener;
+		}
 	}
 	
 	public boolean startServer()
 	{
+		try {
+			if (this.coapServer != null) {
+				this.coapServer.start();
+				
+				for (Endpoint ep : this.coapServer.getEndpoints()) {
+					ep.addInterceptor(new MessageTracer());
+				}
+				
+				return true;
+			} else {
+				_Logger.warning("CoAP server START failed. Not yet initialized.");
+			}
+		} catch (Exception e) {
+			_Logger.log(Level.SEVERE, "Failed to start CoAP server.", e);
+		}
+		
 		return false;
 	}
 	
 	public boolean stopServer()
 	{
+		try {
+			if (this.coapServer != null) {
+				this.coapServer.stop();
+				
+				return true;
+			} else {
+				_Logger.warning("CoAP server STOP failed. Not yet initialized.");
+			}
+		} catch (Exception e) {
+			_Logger.log(Level.SEVERE, "Failed to stop CoAP server.", e);
+		}
+		
 		return false;
 	}
-	
-	
-	// private methods
 	
 	private Resource createResourceChain(ResourceNameEnum resource)
 	{
@@ -93,5 +101,6 @@ public class CoapServerGateway
 	
 	private void initServer(ResourceNameEnum ...resources)
 	{
+		this.coapServer = new CoapServer();
 	}
 }
